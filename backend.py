e5d21a7d76f790d0ef964a81ddf072c44d72a044
aryan2574 <ayush.aryan94@yahoo.com>
2026-08-13 20:12:33 +0200

    Add prompt and agent support for mcp search

--- backend.py
+++ backend.py
@@ -66,6 +66,73 @@ def flight_agent(state: TravelState):
         "llm_calls": state.get("llm_calls", 0) + 1
     }
 
+
+# # Flight tool router prompt
+# FLIGHT_AGENT_PROMPT = """
+# You are a travel flight expert.
+
+# User Query: {query}
+
+# Airport Information: {airport_data}
+
+# Generate:
+# 1. Likely departure airport
+# 2. Likely arrival airport
+# 3. Airlines serving this route
+# 4. Typical flight duration
+# 5. Estimated airfare range
+# 6. Peak season pricing warning
+# 7. Booking advice
+
+# Return concise travel guidance.
+# """
+
+# # Flight agent
+# def flight_agent(state: TravelState):
+#     print("\nINSIDE FLIGHT AGENT")
+
+#     query = state["user_query"]
+
+#     try:
+#         airports = asyncio.run(
+#             aviation_mcp_call(
+#                 "list_airports" #name of the tool
+#             )
+#         )
+
+#         airlines = asyncio.run(
+#             aviation_mcp_call(
+#                 "list_airlines"
+#             )
+#         )
+
+#         print("\nAIRPORTS:", airports)
+#         print("\nAIRLINES:", airlines)
+
+#         prompt = FLIGHT_AGENT_PROMPT.format(
+#             query = query,
+#             airport_data = str(airports)[:3000],
+#             airline_data = str(airlines)[:3000]
+#         )
+
+#         response = llm.invoke([
+#             SystemMessage(content="You are an expert travel flight planner."),
+#             HumanMessage(content=prompt)
+#         ])
+
+#         flight_data = response.content
+
+#     except Exception as e:
+#         flight_data = f"Flight information unavailable: {str(e)}"
+
+#     return {
+#         "flight_results": flight_data,
+#         "messages": [
+#             AIMessage(content="Flight recommendations generated")
+#         ],
+#         "llm_calls":state.get("llm_calls",0) + 1
+#     }
+
 # Hotel Agent
 def hotel_agent(state: TravelState):
     query = f"Best hotels for {state['user_query']}"
